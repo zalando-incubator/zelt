ae94636bd7c3d4003f45e720a70b62bc9bc00728
Brian Maher <brian.maher@zalando.de>
2019-05-21 13:11:33 +0200

    Use git tags over last merge to get last version number.
    Signed-off-by: Brian Maher <brian.maher@zalando.de>

--- check_version.sh
+++ check_version.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
-LAST_MERGE=$(git log --pretty="format:%H" --merges -n 1)
 VERSION_FILE="pyproject.toml"
-PREVIOUS_VERSION=$(git show $LAST_MERGE:$VERSION_FILE | grep version | awk '{print $3}')
-CURRENT_VERSION=$(grep -m 1 version $VERSION_FILE | awk '{print $3}')
+PREVIOUS_VERSION=$(git describe --abbrev=0 --tags)
+# Remove leading 'v' char.
+PREVIOUS_VERSION=${PREVIOUS_VERSION#?}
+# Using sed to remove leading and trailing double quotes.
+CURRENT_VERSION=$(grep -m 1 version $VERSION_FILE | awk '{print $3}' | sed -e 's/^"//' -e 's/"$//')
 
 if [ $CURRENT_VERSION == $PREVIOUS_VERSION ]; then
   echo "ERROR: Version has not been updated in '$VERSION_FILE'!"
